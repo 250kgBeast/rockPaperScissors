cb49a4630b6028c5cc0e230b3d3b0e5f3384576c
250kgBeast <bakarov@ukr.net>
2020-05-04 21:23:16 +0300

    added random

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,21 +1,17 @@
 import random
 
-
-x = input()
-random_integer = random.randint(1, 3)
-computer_choice = 0
-if random_integer == 1:
-    computer_choice = "rock"
-elif random_integer == 2:
-    computer_choice = "scissors"
+options = ['rock', 'paper', 'scissors']
+computer = random.choice(options)
+player = input()
+while player != "!exit":
+    if player == computer:
+        print(f"There is a draw ({player})")
+    elif player == options[options.index(computer) - 2]:
+        print(f"Well done. Computer chose {computer} and failed")
+    elif player == options[options.index(computer) - 1]:
+        print(f"Sorry, but computer chose {computer}")
+    elif player not in options:
+        print("Invalid input")
+    player = input()
 else:
-    computer_choice = "paper"
-
-if x == computer_choice:
-    print(f"There is a draw ({x})")
-elif (x == "scissors" and computer_choice == "paper")\
-        or (x == "paper" and computer_choice == "rock")\
-        or (x == "rock" and computer_choice == "scissors"):
-    print(f"Well done. Computer chose {computer_choice} and failed")
-else:
-    print(f"Sorry, but computer chose {computer_choice}")
+    print("Bye!")
